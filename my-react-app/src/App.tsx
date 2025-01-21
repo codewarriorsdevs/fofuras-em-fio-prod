5d8ee9ca58d49b34406f0dc3599d99b40cb06541
kelySoare5 <kelysoares688@gmail.com>
2025-01-21 12:38:57 -0500

    seção header

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Header } from "./components/header/Header"
 
 
 function App() {
@@ -5,14 +6,7 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white p-4">
-        <h1 className="text-4xl font-bold text-center mb-4 text-shadow-lg">
-          Projeto Boladão da Comunidade!
-        </h1>
-        <h4 className="text-lg font-semibold text-center">
-          Seja bem-vindo e ben-vinda :)
-        </h4>
-      </div>
+      <Header/>
 
     </>
   )
